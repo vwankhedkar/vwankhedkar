0b25e0afc5c91fb0cc624d3d23afb1cb74c6b335
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-10-26 20:13:09 +0530

    Create row_sum_odd_numbers.py

---
+++ row_sum_odd_numbers.py
@@ -0,0 +1,21 @@
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+import math
+def rowSumOddNumbers(n):
+    return math.pow(n, 3)
+
+if __name__ == '__main__':
+    print (rowSumOddNumbers(3))
+    
+  ********************************************
+  
+ import math
+def rowSumOddNumbers(n):
+    return n * n * n
+
+if __name__ == '__main__':
+    print (rowSumOddNumbers(3))
+  
