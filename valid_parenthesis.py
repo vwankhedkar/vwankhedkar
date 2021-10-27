7663447bcf21dfc499a9607128c5171ea80f7bd7
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-10-27 20:05:07 +0530

    Update valid_parenthesis.py

--- valid_parenthesis.py
+++ valid_parenthesis.py
@@ -1,4 +1,72 @@
 def valid_parentheses(string):
+    string = "".join(ch for ch in string if ch in "()")
+    while "()" in string: string = string.replace("()", "")
+    return not string
+******************************************************************
+def valid_parentheses(string):
+    cnt = 0
+    for char in string:
+        if char == '(' or char == '[' or char == '{': cnt += 1
+        if char == ')' or char == ']' or char == ']': cnt -= 1
+        if cnt < 0: return False
+    return True if cnt == 0 else False
+  
+# Driver Code 
+if __name__ == "__main__": 
+    string = "[[]][]()"
+    string = list(string) 
+  
+    print("Yes") if valid_parentheses(string) else print("No") 
+*********************************************************************
+class Stack:
+    """
+    This is an implementation of a Stack.
+    The "right" or "top" of this stack the end of the list.
+    """
+
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def is_empty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+def valid_parentheses(symbol_string):
+    """
+    This is a practice problem.
+    It checks to see if parenthesis's are balanced
+    :param symbol_string: String
+    :return Bool:
+    """
+
+    stack = Stack()
+    for char in symbol_string:
+        if char == "(":
+            stack.push("(")
+        elif char == ")":
+            if stack.is_empty():
+                return False
+            else:
+                stack.pop()
+
+    if not stack.is_empty():
+        return False
+    else:
+        return True
+*********************************************************************
+
+def valid_parentheses(string):
     dict = {'(':')','{':'}','[':']'}
     stack = []
     for ch in string:
