a1c0fff9deabe4ac3c188fafd595d765de4b2aac
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-10-27 14:01:30 +0530

    Update Char_count.py

--- Char_count.py
+++ Char_count.py
@@ -10,3 +10,33 @@ def str_to_paren(str):
 if __name__ == '__main__':
     str = "ababdede"
     print(str_to_paren(str))
+
+char count 
+--------
+st ="happyprogrampppming"
+ch = "p"
+dir = {}
+
+for ele in st:
+    if ele not in dir:
+        dir[ele] = 1
+    else:
+        dir[ele] += 1
+
+for k, v in dir.items():
+    # print("Character : ", k , "Appears: ", v, "times")
+    if (ch == k):
+        print("Character : ", k , "Appears: ", v, "times")
+        
+vowels count -->
+
+string ="absdauoi"
+char, vow = 0, 0 
+
+for ele in string:
+    if ele in ['a','e','i','o','u']:
+        vow += 1
+    else:
+        char += 1
+
+print("Character : ", char , "vowels: ", vow)
