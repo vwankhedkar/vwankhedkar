0b84ba41d5f13fd6bf43404a9583434ab18a0f92
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-10-30 20:23:20 +0530

    Update getTrappedRainwater.py

--- getTrappedRainwater.py
+++ getTrappedRainwater.py
@@ -16,3 +16,24 @@ def getTrappedRainwater(heights):
 
 elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
 print(getTrappedRainwater(elevationArray))
+*****************************************************************
+def getTrappedRainwater(heights):
+    left=0
+    right=len(heights)-1
+    totalWater, maxLeft, maxRight = 0, 0, 0
+    while(left < right):
+        if (heights[left] <= heights[right]):
+            if heights[left] >= maxLeft:
+                maxLeft = heights[left]
+            else:
+                totalWater += maxLeft - heights[left]
+            left += 1
+        else:
+            if heights[right] >= maxRight:
+                maxRight = heights[right]
+            else:
+                totalWater += maxRight - heights[right]
+            right -= 1 
+    return totalWater
+elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
+print(getTrappedRainwater(elevationArray))
