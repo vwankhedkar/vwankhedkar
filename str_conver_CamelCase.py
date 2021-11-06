943ca44460875c368ae8844cfa51a184064c9023
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-11-06 21:13:56 +0530

    Update str_conver_CamelCase.py

--- str_conver_CamelCase.py
+++ str_conver_CamelCase.py
@@ -30,4 +30,11 @@ if __name__ == '__main__':
     # s = accum("abcd")
     s = accum("RqaEzty")
     print(s)
+**********************************************************************************
 
+test.describe("Testing function to_camel_case")
+test.it("Basic tests")
+test.assert_equals(to_camel_case(''), '', "An empty string was provided but not returned")
+test.assert_equals(to_camel_case("the_stealth_warrior"), "theStealthWarrior", "to_camel_case('the_stealth_warrior') did not return correct value")
+test.assert_equals(to_camel_case("The-Stealth-Warrior"), "TheStealthWarrior", "to_camel_case('The-Stealth-Warrior') did not return correct value")
+test.assert_equals(to_camel_case("A-B-C"), "ABC", "to_camel_case('A-B-C') did not return correct value")
