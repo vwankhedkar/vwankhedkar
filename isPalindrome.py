66ae08cff493c272eb43ffa63e8ec8e9e8d14fdc
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-11-08 10:05:29 +0530

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -16,3 +16,18 @@ class Solution:
 string = "A man, a plan, a canal: Panama"
 b = Solution()
 print(b.isPalindrome(string))
+**********************************************************
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        pattern = r'[^A-Za-z0-9]+'
+        s = re.sub(pattern, '', s).lower()
+        rev = ""
+        for i in range(len(s)):
+            rev += s[i]
+            i -= 1
+        return rev == s
+
+string = "A man, a plan, a canal: Panama"
+b = Solution()
+print(b.isPalindrome(string))
