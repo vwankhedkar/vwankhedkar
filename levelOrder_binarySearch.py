7372866e1cf8e412836afc06194e6ed6e8aeb90c
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-11-09 18:28:36 +0530

    Update levelOrder_binarySearch.py

--- levelOrder_binarySearch.py
+++ levelOrder_binarySearch.py
@@ -53,3 +53,85 @@ if __name__ == "__main__":
     s = Solution().levelOrder(root)
     print("Level Order Traversal of binary tree is -")
     print(s)
+***************************************************************
+DFS - right side reversal 
+# Time:  O(n)
+# Space: O(h)
+#
+# Given a binary tree, imagine yourself standing on the right side of it, 
+# return the values of the nodes you can see ordered from top to bottom.
+#
+# For example:
+# Given the following binary tree,
+#    1            <---
+#  /   \
+# 2     3         <---
+#  \     \
+#   5     4       <---
+# You should return [1, 3, 4].
+#
+
+# Definition for a  binary tree node
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+class Solution:
+    def rightSideView(self, root):
+        result = []
+        self.rightSideViewDFS(root, 1, result)
+        return result
+
+    def rightSideViewDFS(self, node, depth, result):
+        if not node:
+            return
+        if depth > len(result):
+            result.append(node.val)
+        self.rightSideViewDFS(node.right, depth+1, result)
+        self.rightSideViewDFS(node.left, depth+1, result)
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    s = Solution().rightSideView(root)
+    print("Binary tree right side view traversal is -")
+    print(s)
+    
+  **********************************************************
+BFS 
+class TreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+class Solution:
+    def rightSideView(self, root):
+        if root is None:
+            return []
+
+        result, current = [], [root]
+        while current:
+            next_level = []
+            for i, node in enumerate(current):
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+                if i == len(current) - 1:
+                    result.append(node.val)
+            current = next_level
+        return result
+
+if __name__ == "__main__":
+    root = TreeNode(3)
+    root.left = TreeNode(9)
+    root.right = TreeNode(20)
+    root.right.left = TreeNode(15)
+    root.right.right = TreeNode(7)
+    s = Solution().rightSideView(root)
+    print("Binary tree right side view BFS traversal is -")
+    print(s)
