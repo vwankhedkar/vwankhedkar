dca7885fe79f600ece4f3d9eb5445674e0972d7f
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2021-12-10 09:43:15 +0530

    Create doubleClick.py

---
+++ Selenium/doubleClick.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ActionChains
+import time
+
+opt = Options()
+opt.add_argument("--disable-notifications")
+driver = webdriver.Chrome(executable_path=r"D:\Selenium\chromedriver.exe",options=opt)
+driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver")
+driver.maximize_window()
+action.double_click(driver.find_element_by_id("double-click")).perform()
+alert = driver.switch_to.alert
+print(alert.text)
+alert.accept()
