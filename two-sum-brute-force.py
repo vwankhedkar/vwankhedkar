139d5b872636680e0bc68e01c5498a9efd6190dd
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2022-07-09 22:34:25 +0530

    Update two-sum-brute-force.py

--- two-sum-brute-force.py
+++ two-sum-brute-force.py
@@ -10,5 +10,19 @@ arr = [9, 7, 2, 4, 6, 8, 2, 1, 5]
 target = 14
 print(findTwoSum(arr, target))
 
+OUTPUT : (0, 8)
 ****************************************
 
+def find_sum_twonum(nums, target):    
+    for p1 in range(len(nums)):
+        num_to_find = target - nums[p1]
+        for p2 in range(p1+1, len(nums)):
+            if (num_to_find == nums[p2]):
+                return p1, p2, nums[p1], nums[p2] 
+nums = [1,3,7,9,2]
+target = 11
+loc_P1, loc_p2, n1, n2 = find_sum_twonum(nums, target)
+print("Sum of numbers {} {} found at locations nums[{}] nums[{}]".format(n1, n2, loc_P1, loc_p2))
+
+OUTPUT :-
+    Sum of numbers 9 2 found at locations nums[3] nums[4]
