e16ef5284b51842ac951cbe45159f1c7056ed3e6
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2022-07-14 13:53:02 +0530

    Update Leetcode_TypedOutString.py

--- Leetcode_TypedOutString.py
+++ Leetcode_TypedOutString.py
@@ -43,3 +43,35 @@ if res1 == res2:
 else:
    print("False")
 ------------------------------------------
+Failing with given o/p 
+def backspaceCompare(s, t):
+   p1 = len(s)-1
+   p2 = len(t)-1
+   while (p1 >= 0 or p2 >= 0):
+      if (s[p1] == '#' or t[p2] == '#'):
+         if s[p1] == '#':
+            backCount = 2
+            while (backCount > 0):
+               p1 -= 1
+               backCount -= 1
+               if (s[p1] == '#'):
+                  backCount += 2
+         if (t[p2] == '#'):
+            backCount = 2
+            while (backCount > 0):
+               p2 -= 1
+               backCount -= 1
+               if (t[p2] == '#'):
+                  backCount += 2
+      else:
+         if (s[p1] != t[p2]):
+            return False
+         else:
+            p1 -= 1
+            p2 -= 1   
+   return True
+
+s = "ab#c"
+t = "ad#c"
+print(backspaceCompare(s, t))
+
