7244b7c621ef01e1e48c3a40f6b6c47a738278f1
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2022-07-17 21:05:53 +0530

    Update minRemoveToMakeValid.py

--- minRemoveToMakeValid.py
+++ minRemoveToMakeValid.py
@@ -1,3 +1,77 @@
+# Time:  O(C(n, c)), try out all possible substrings with the minimum c deletion.
+# Space: O(c), the depth is at most c, and it costs n at each depth
+#
+# Remove the minimum number of invalid parentheses in order to
+# make the input string valid. Return all possible results.
+#
+# Note: The input string may contain letters other than the
+# parentheses ( and ).
+#
+# Examples:
+# "()())()" -> ["()()()", "(())()"]
+# "(a)())()" -> ["(a)()()", "(a())()"]
+# ")(" -> [""]
+#
+
+# DFS solution.
+class Solution(object):
+    def removeInvalidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        # Calculate the minimum left and right parantheses to remove
+        def findMinRemove(s):
+            left_removed, right_removed = 0, 0
+            for c in s:
+                if c == '(':
+                    left_removed += 1
+                elif c == ')':
+                    if not left_removed:
+                        right_removed += 1
+                    else:
+                        left_removed -= 1
+            return (left_removed, right_removed)
+
+        # Check whether s is valid or not.
+        def isValid(s):
+            sum = 0
+            for c in s:
+                if c == '(':
+                    sum += 1
+                elif c == ')':
+                    sum -= 1
+                if sum < 0:
+                    return False
+            return sum == 0
+
+        def removeInvalidParenthesesHelper(start, left_removed, right_removed):
+            if left_removed == 0 and right_removed == 0:
+                tmp = ""
+                for i, c in enumerate(s):
+                    if i not in removed:
+                        tmp += c
+                if isValid(tmp):
+                    res.append(tmp)
+                return
+    
+            for i in xrange(start, len(s)):
+                if right_removed == 0 and left_removed > 0 and s[i] == '(':
+                    if i == start or s[i] != s[i - 1]:  # Skip duplicated.
+                        removed[i] = True
+                        removeInvalidParenthesesHelper(i + 1, left_removed - 1, right_removed)
+                        del removed[i]
+                elif right_removed > 0 and s[i] == ')':
+                    if i == start or s[i] != s[i - 1]:  # Skip duplicated.
+                        removed[i] = True
+                        removeInvalidParenthesesHelper(i + 1, left_removed, right_removed - 1);
+                        del removed[i]
+
+        res, removed = [], {}
+        (left_removed, right_removed) = findMinRemove(s)
+        removeInvalidParenthesesHelper(0, left_removed, right_removed)
+        return res
+==============================================================
 class Solution(object):
   def minRemoveToMakeValid(self, s: str) -> str:
         removed_ind = set()
@@ -68,3 +142,38 @@ class Solution(object):
 s = "}a)]b[(c)]{d}{"
 l = Solution()
 print(l.minRemoveToMakeValid(s))
+
+==============================================
+class Solution:
+   def minRemoveToMakeValid(self, s):
+      stack = []
+      res = list(s)
+      output = ''
+      for i in range(len(s)):
+         if s[i] == '(' and len(s)>=0:
+            stack.append(i)
+         elif s[i] == ')' and len(stack)>0:
+            stack.pop()
+         elif s[i] == ')':
+            res[i] = ""
+            output = "".join(res)
+
+      while len(stack):
+         curIndx = stack.pop()
+         res[curIndx] = ""
+         output = "".join(res)
+      return output 
+
+string1 = "a))c((b"
+# string1 = "a)b(c)d"    
+# string1 = "lee(t(c)o)de)"
+# string1 = "))(("
+# print(string1.replace(')', ""))
+print(Solution().minRemoveToMakeValid(string1))
+
+OUPUT :
+ab(c)d
+PS D:\ESH_AUTOMATION> & C:/Programs/Python/Python39/python.exe d:/ESH_AUTOMATION/webUI/try.py
+lee(t(c)o)de
+PS D:\ESH_AUTOMATION> & C:/Programs/Python/Python39/python.exe d:/ESH_AUTOMATION/webUI/try.py
+acb
