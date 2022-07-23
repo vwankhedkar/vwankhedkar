022e81b1e72961118f6ef397555d1fabf0c00838
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2022-07-23 20:30:48 +0530

    Update isPowerOfTwo.py

--- isPowerOfTwo.py
+++ isPowerOfTwo.py
@@ -39,7 +39,20 @@ if(isPowerOfTwo(64)):
     print("Yes");
 else:
     print("No");
+----------------------------------------------------------
+def isPowerOfTwo(x):
+    return (x and (not (x & (x-1))))
+
+if(isPowerOfTwo(31)):
+    print("Yes");
+else:
+    print("No");
  
+if(isPowerOfTwo(64)):
+    print("Yes");
+else:
+    print("No");
+ --------------------------------------------------------------
 OUTPUT:
   No
   Yes
