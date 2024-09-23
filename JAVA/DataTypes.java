da782a1a1a063e5974bc121bb394f5deb70e182a
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2024-09-23 09:31:35 +0530

    Update DataTypes.java

--- JAVA/DataTypes.java
+++ JAVA/DataTypes.java
@@ -1,3 +1,4 @@
+for b = 10.5 & 128 values /tmp/n8m0g4uaM4/Test.java:6: error: incompatible types: possible lossy conversion from double to byte
 class Test {
     public static void main(String[] args) {
         byte b = 12;
