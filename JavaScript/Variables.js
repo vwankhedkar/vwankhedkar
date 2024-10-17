509b637b0b53e385640ad3a01288e77a90a8e6f9
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2024-10-17 13:34:24 +0530

    Create Variables.js

---
+++ JavaScript/Variables.js
@@ -0,0 +1,33 @@
+console.log("Hello")
+
+let num = 4
+console.log(num)
+
+let username = "Vais"
+console.log(username)
+
+let username = "Vais's Account"
+console.log(username)
+
+let username = 'Vaisali'
+console.log(username)
+
+OUTPUT
+
+PS C:\Users\VWankhedkar\PycharmProjects\Robot\TestCase> node first.js
+Hello
+4
+Vais
+Vais's Account
+Vaisali
+
+**********************************************
+let radius = 5
+const pi = 3.14
+let area
+radus = 6
+area = pi * radius * radius
+console.log(area)
+
+OUTPUT - 78.5
+*************************************************
