2ac518f89cad68b1eea85fd2a13c2e95b4bc975f
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2024-10-22 19:49:54 +0530

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,3 +1,23 @@
+def ispalin(str):
+    for i in range(0, int(len(str)/2)):
+        if str[i] != str[len(str)-i-1]:
+            return False
+        return True
+s = "malayalam"
+ans = ispalin(s)
+if (ans):
+    print("String is Panildrome")
+else:
+    print("String is not Panildrome")
+
+def ispalin(str):
+    return str == str[::-1]
+
+PS C:\Users\VWankhedkar\PycharmProjects\Robot\TestCase> python.exe .\Hello.py
+String is Panildrome
+
+*************************************************************
+
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
