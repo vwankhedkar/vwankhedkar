e88c09cd1cc0a08371050b2a15de9acd839cddca
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2024-12-17 16:28:47 +0530

    Update and rename recFactorial.py to Python/recFactorial.py

--- Python/recFactorial.py
+++ Python/recFactorial.py
@@ -19,3 +19,16 @@ print(tailFactorial(5))
 
 OUTPUT : 120
 
+memo = {}
+def fact(n):
+    if n == 0:
+        return 1
+    if n in memo:
+        return memo[n]
+    memo[n] = n * fact(n-1)
+    return memo[n]
+print(fact(5))
+print(memo)
+OUTPUT
+120
+{1: 1, 2: 2, 3: 6, 4: 24, 5: 120}
