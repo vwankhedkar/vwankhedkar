2cf2d7f42e6b7914e0f0d2ce22de446f7d8ed26d
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2024-12-17 16:35:03 +0530

    Create randomNum.py

---
+++ Python/randomNum.py
@@ -0,0 +1,14 @@
+import random
+print(random.random())
+print(random.uniform(1,100))
+print(random.randint(1,100))
+print(random.randrange(0, 100, 2)) # print even random number
+print(random.sample(range(0,100),3))
+
+C:\Users\VWankhedkar\PycharmProjects\Practise-Proj\venv\Scripts\python.exe C:\Users\VWankhedkar\PycharmProjects\Practise-Proj\moveZeros.py 
+0.032447537613006894
+98.56763499441112
+27
+16
+[73, 36, 22]
+
