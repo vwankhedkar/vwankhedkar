6dd2d54e9aba3d25a82922089810b6e87fdea20c
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-01-03 12:20:53 +0530

    Update and rename wordcount_file.py to files_ops.py

--- Python/files_ops.py
+++ Python/files_ops.py
@@ -1,3 +1,13 @@
+Files copy
+f1 = open('file1.txt','r')
+f2 = open('file2.txt','w')
+data = f1.read()
+f2.write(data)
+f1.close()
+f2.close()
+print("Data copied successfully ....")
+Data copied successfully ....
+***************************************************************************************
 file = open("D:\\python-webtraining\\scripts\\Algorithms\\temp_file.txt", 'r')
 dir = {}
 
