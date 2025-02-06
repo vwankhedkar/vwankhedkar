277420d2064b78906bfb945edb041838f2127a91
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-02-06 14:56:50 +0530

    Update recFactorial.py

--- Python/recFactorial.py
+++ Python/recFactorial.py
@@ -32,3 +32,32 @@ print(memo)
 OUTPUT
 120
 {1: 1, 2: 2, 3: 6, 4: 24, 5: 120}
+*********************************************
+Nonrecursive
+def compute_factorial(num):
+    factorial = 1
+    if num < 0:
+        print("Factorial does not exists for negative numbers")
+    elif num == 0:
+        print("Factorial of 0 = 1")
+    else:
+        for i in range(1, num+1):
+            factorial = factorial * i
+        print("The factorial of ",num, "is: ", factorial)
+compute_factorial(int(input("Enter a number: ")))
+Enter a number: 4
+The factorial of  4 is:  24
+******************************************************
+memo = {}
+def factorial_memory(n):
+    if n == 0:
+        return 1
+    if n in memo:
+        return memo[n]
+    memo[n] = n * factorial_memory(n-1)
+    return memo[n]
+factorial_memory(int(input("Enter a number: ")))
+print(memo)
+OUTPUT
+Enter a number: 6
+{1: 1, 2: 2, 3: 6, 4: 24, 5: 120, 6: 720}
