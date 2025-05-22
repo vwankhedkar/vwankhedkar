b42ac6f594034bf7af3b8585ddcb3ffb10b053cf
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-05-22 20:38:57 +0530

    Create csv_ops_request-json.py

---
+++ Python/csv_ops_request-json.py
@@ -0,0 +1,33 @@
+import csv
+with open("myfile.csv","r") as file:
+    csvFile = csv.reader(file)
+    for lines in csvFile:
+        print(lines)
+print("*****************************************")
+with open("myfile.csv","r") as file:
+    csvFile = csv.DictReader(file)
+    for lines in csvFile:
+        print(lines)
+print("*****************************************")
+rows = []
+with open("myfile.csv","r") as file:
+    csvFile = csv.reader(file)
+    header = next(csvFile)
+    for lines in csvFile:
+        rows.append(lines)
+print(rows)
+for row in rows:
+    print(row)
+['user', 'city', 'email']
+['jake', 'Lucas', 'jake@mymail.com']
+['lucas', 'London.lucas@mymail.com']
+['Barcelona', 'Doha', 'Barcelona@mymail.com']
+*****************************************
+{'user': 'jake', 'city': 'Lucas', 'email': 'jake@mymail.com'}
+{'user': 'lucas', 'city': 'London.lucas@mymail.com', 'email': None}
+{'user': 'Barcelona', 'city': 'Doha', 'email': 'Barcelona@mymail.com'}
+*****************************************
+[['jake', 'Lucas', 'jake@mymail.com'], ['lucas', 'London.lucas@mymail.com'], ['Barcelona', 'Doha', 'Barcelona@mymail.com']]
+['jake', 'Lucas', 'jake@mymail.com']
+['lucas', 'London.lucas@mymail.com']
+['Barcelona', 'Doha', 'Barcelona@mymail.com']
