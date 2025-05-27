2ab6f508bac8ea0e723128c34be412cb2aabd708
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-05-27 13:47:38 +0530

    Update ReadFile.py

--- REST_API/ReadFile.py
+++ REST_API/ReadFile.py
@@ -20,3 +20,50 @@ def update_user_info(file_name, name=None, email=None, phone=None):
         user_info['phone'] = phone
 
     return user_info
+******************************************************
+email,password
+auto1@auto,1234
+auto2@auto,1234
+auto3@auto,1234
+auto4@auto,1234
+          
+fileutils.py    
+import csv
+import json
+from pathlib import Path
+import random
+BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
+TEST_DATA_DIR = BASE_DIR.joinpath('TestData')
+def getJsonFromFile(filename):
+    filepath = TEST_DATA_DIR.joinpath(filename)
+    with open(filepath,'r') as file:
+        return json.load(file)
+
+def getCsvDataAsDict(filename):
+    filepath = TEST_DATA_DIR.joinpath(filename)
+    with open(filepath, 'r') as file:
+        csvFile = csv.DictReader(file)
+        dictList = list(csvFile)
+    return dictList
+print("--------------")
+print(getCsvDataAsDict('registerAPIData.csv'))
+
+def getDataAsList(filename):
+    filepath = TEST_DATA_DIR.joinpath(filename)
+    with open(filepath, 'r') as csvFile:
+        reader = csv.reader(csvFile)
+        next(reader)
+        lines = list(reader)
+    return lines
+print("--------------")
+print(getDataAsList('registerAPIData.csv'))
+
+
+C:\Trainings\Pytest-Bdd-Udemy\TestFrameworkApp-main\.venv\Scripts\python.exe C:\Users\vwank\PycharmProjects\PytestFramework\utils\fileUtils.py 
+C:\Users\vwank\PycharmProjects\PytestFramework
+--------------
+[{'email': 'auto1@auto', 'password': '1234'}, {'email': 'auto2@auto', 'password': '1234'}, {'email': 'auto3@auto', 'password': '1234'}, {'email': 'auto4@auto', 'password': '1234'}]
+--------------
+[['auto1@auto', '1234'], ['auto2@auto', '1234'], ['auto3@auto', '1234'], ['auto4@auto', '1234']]
+
