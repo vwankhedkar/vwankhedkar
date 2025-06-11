0e0f38934e5c45eb433f3346d4a9393b043a13cb
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-06-11 14:04:31 +0530

    Create smallCodes.py

---
+++ Python/smallCodes.py
@@ -0,0 +1,25 @@
+import decimal
+integer = 10
+print(decimal.Decimal(integer))
+print(type(decimal.Decimal(integer)))
+string = '12345'
+print(decimal.Decimal(string))
+print(type(decimal.Decimal(string)))
+C:\Trainings\Pytest-Bdd-Udemy\TestFrameworkApp-main\.venv\Scripts\python.exe C:\Users\vwank\PycharmProjects\PytestFramework\Python_Codes\try.py 
+10
+<class 'decimal.Decimal'>
+12345
+<class 'decimal.Decimal'>
+************************************************************************************************************
+
+
+************************************************************************************************************
+
+
+************************************************************************************************************
+
+
+************************************************************************************************************
+
+
+************************************************************************************************************
