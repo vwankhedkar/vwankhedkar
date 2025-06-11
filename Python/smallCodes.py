98eac559ebb004d9c891ac83523c2b384403d5d7
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-06-11 15:00:10 +0530

    Update smallCodes.py

--- Python/smallCodes.py
+++ Python/smallCodes.py
@@ -11,11 +11,44 @@ C:\Trainings\Pytest-Bdd-Udemy\TestFrameworkApp-main\.venv\Scripts\python.exe C:\
 12345
 <class 'decimal.Decimal'>
 ************************************************************************************************************
+str = "Python Programming"
+print(str[::-1])
 
+vowels = ['a', 'e', 'i', 'o', 'u']
+vowel_count = 0
+vowel_dict = {}
+const_count = 0
+const = []
+for ch in str:
+    if (ch in vowels or ch == ' '):
+        vowel_dict[ch] = vowel_dict.get(ch, 0) + 1
+        vowel_count += 1
+    else:
+        const_count += 1
+        const.append(ch)
 
+print(vowel_dict, "total vowels :=> ", vowel_count, "total constants :=> ", const_count, const)
+gnimmargorP nohtyP
+{'o': 2, ' ': 1, 'a': 1, 'i': 1} total vowels :=>  5 total constants :=>  13 ['P', 'y', 't', 'h', 'n', 'P', 'r', 'g', 'r', 'm', 'm', 'n', 'g']
 ************************************************************************************************************
+fib = [0,1]
+n = 5
+for i in range(n):
+    fib.append(fib[-1] + fib[-2])
+print(', '.join(str(e) for e in fib))  ===>  0, 1, 1, 2, 3, 5, 8
 
-
+import re
+name = 'Python is 1'
+digitCount = re.sub("[^0-9]", "",name)
+letterCount = re.sub("[^a-zA-Z]", "", name)
+spaceCount = re.findall(r"[ \s]", name)
+print(digitCount, len(digitCount))
+print(letterCount, len(letterCount))
+print(len(spaceCount))
+C:\Trainings\Pytest-Bdd-Udemy\TestFrameworkApp-main\.venv\Scripts\python.exe C:\Users\vwank\PycharmProjects\PytestFramework\Python_Codes\try.py 
+1 1
+Pythonis 8
+2
 ************************************************************************************************************
 
 
