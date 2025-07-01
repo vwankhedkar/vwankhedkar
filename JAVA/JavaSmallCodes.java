9dea1a4a6e558d6857447fec813009cd549f1aed
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-07-01 22:01:51 +0530

    Update JavaSmallCodes.java

--- JAVA/JavaSmallCodes.java
+++ JAVA/JavaSmallCodes.java
@@ -1585,5 +1585,47 @@ public class tryProgs2 {
 5x8=40
 5x9=45
 *********************************************************************************
+package com.tryPrograms;
+public class Try1 {
+    public void finalize() { System.out.println("Object is garbage collected...");}
+    public static void main(String[] args) {
+    	Try1 s1 = new Try1();
+       	Try1 s2 = new Try1();
+    	s1 = null;
+    	s2 = null;
+    	System.gc();
+    }
+}
+Object is garbage collected...
+Object is garbage collected...
+*********************************************************************************
+package com.tryPrograms;
+import java.util.Arrays;
+public class Try1 {
+    public static void main(String[] args) {
+    	int[] nums = {1,2,3,4,5,6,7};
+    	int k = 3;
+    	if (k > nums.length) {
+    		k = k % nums.length;  }
+    	int[] result = new int[nums.length];	
+    	for (int i=0; i<k; i++) {
+    		result[i] = nums[nums.length-k+i];
+    	}
+    	int j = 0;
+    	for (int i=k; i<nums.length; i++) {
+    		result[i] = nums[j];
+    		j++;
+    	}
+    	System.arraycopy(result, 0, nums, 0, nums.length);
+    	System.out.println(Arrays.toString(nums));
+    }
+}   ====>	[5, 6, 7, 1, 2, 3, 4]
+*********************************************************************************
+
+*********************************************************************************
+
+*********************************************************************************
+
+*********************************************************************************
 
 *********************************************************************************
