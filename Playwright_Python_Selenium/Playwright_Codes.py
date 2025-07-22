16a12e6a2ab491c2cf442829e0e0fe62d6c8ec60
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-07-22 21:17:31 +0530

    Update Playwright_Codes.py

--- Playwright_Python_Selenium/Playwright_Codes.py
+++ Playwright_Python_Selenium/Playwright_Codes.py
@@ -29,9 +29,67 @@ with sync_playwright() as play:
     loginbutton.click()
     page.wait_for_timeout(3000)
 --------------------------------------------------------------------------------------
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    username_element = page.wait_for_selector('//input[@name="username"]')
+    username_element.type('Admin')
+    password_element = page.wait_for_selector('//input[@placeholder="Password"]')
+    password_element.type('admin123')
+    login_element = page.wait_for_selector('//button[@type="submit"]')
+    login_element.click()
+    page.wait_for_timeout(3000)
 --------------------------------------------------------------------------------------
+import time
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://demo.automationtesting.in/Register.html')
+    # select_dropdown = page.query_selector('//select[@id="Skills"]')
+    # time.sleep(1)
+    # select_dropdown.select_option(label='Art Design')
+    page.select_option('//select[@id="Skills"]',label='AutoCAD')
+    time.sleep(2)
+    page.wait_for_timeout(3000)
 --------------------------------------------------------------------------------------
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    username_element = page.wait_for_selector('//input[@name="username"]')
+    username_element.type('Admin')
+    password_element = page.wait_for_selector('//input[@placeholder="Password"]')
+    password_element.type('admin123')
+    login_element = page.wait_for_selector('//button[@type="submit"]')
+    login_element.click()
+    page.wait_for_timeout(3000)
+    # page.wait_for_selector('//p[text()="Forgot your password? "]').click()
+    # page.wait_for_timeout(3000)
+    # locators
+    # //input[contains(@placeholder,"Username")] and //label[contains(text(), "Username")]
+    # family
+    # parent - //tagname[@id="xy"]/parent::input[]
+    # child - //tagname[@id="xy"]/child::input[]
+    # ancestor
+    # sibling - //td[text()="Microsoft"]//following-sibling:: td[2]
 --------------------------------------------------------------------------------------
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto('https://demo.automationtesting.in/Register.html')
+    radio_button = page.query_selector('//input[@value="FeMale"]')
+    radio_button.click()
+    radio_button.check()
+    if radio_button.is_checked():
+        print("Passed")
+    else:
+        print("Failed")
+    page.wait_for_timeout(3000)
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
