590579f38589d08c6e5860742f2b7487f08f973d
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-07-26 16:19:52 +0530

    Update OOPs_Collection.java

--- JAVA/OOPs_Collection.java
+++ JAVA/OOPs_Collection.java
@@ -814,6 +814,28 @@ public class Student {
 Enter a string: Java is programming language
 Reversed String is : egaugnal gnimmargorp si avaJ
 ************************************************************************************
+package com.tryPrograms;
+import java.util.Scanner;
+public class Student {
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter any number : ");
+		int number = sc.nextInt();
+		if (isPrime(number))
+			System.out.println(number + " is prime");
+		else
+			System.out.println(number + " is not prime");
+	}
+	public static boolean isPrime(int num) {
+		for (int i=2; i<= num/2; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+}
+Enter any number : 17
+17 is prime
 ************************************************************************************
 ************************************************************************************
 ************************************************************************************
