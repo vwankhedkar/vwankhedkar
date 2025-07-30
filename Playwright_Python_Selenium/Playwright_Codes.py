7fa3db4401830c22250dabc96ec0836076308301
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-07-30 10:21:06 +0530

    Update Playwright_Codes.py

--- Playwright_Python_Selenium/Playwright_Codes.py
+++ Playwright_Python_Selenium/Playwright_Codes.py
@@ -208,8 +208,24 @@ with sync_playwright() as p:
     page.wait_for_selector('//a[text()="SwitchTo"]').press("$")
     page.wait_for_timeout(2000)
 --------------------------------------------------------------------------------------
-
+from playwright.sync_api import sync_playwright
+def handle_rejex(response):
+    if 'https://www.plus2net.com/php_tutorial/dd-ajax.php?' in response.url:
+        status = response.status
+        data = response.text()
+        print(f'status:{status},data{data}')
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto('https://www.plus2net.com/php_tutorial/ajax_drop_down_list-demo.php')
+    select = page.wait_for_selector('//select[@id="s1"]')
+    page.on('response', lambda response:handle_rejex(response))
+    select.select_option('1')
+    page.wait_for_timeout(2000)
+status:200,data{"data":[{"subcategory":"Mango","subcat_id":1},{"subcategory":"Banana","subcat_id":2},{"subcategory":"Orange","subcat_id":3},{"subcategory":"Apple","subcat_id":4}]}
 --------------------------------------------------------------------------------------
+
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------
