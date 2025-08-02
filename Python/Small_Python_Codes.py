faf0c2fe466dbe5b7a54d08569a29db31c8043fe
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-02 16:38:10 +0530

    Update and rename smallCodes.py to Small_Python_Codes.py

--- Python/Small_Python_Codes.py
+++ Python/Small_Python_Codes.py
@@ -735,5 +735,26 @@ for str in words:
 print("Words count are : ", wrd_cnt)
 Words count are :  {'Kundan': 2, 'Kumar': 2, 'Pandey': 1}
 ***********************************************************************
+# Convert from word to digit
+
+words_upto_19 = ['','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGHT',
+                 'NINE','TEN','ELEVEN','TWELVE','THIRTEEN','FOURTEEN','FIFTEEN',
+                 'SIXTEEN','SEVENTEEN','EIGHTEEN','NINETEEN']
+words_upto_tens = ['','','TWENTY','THIRTY','FOURTY','FIFTY','SIXTY',
+                  'SEVENTY','EIGHTY','NINETY']
+
+n = int(input("Enter a digit from 0-99: "))
+output=''
+if n==0:
+    output='ZERO'
+elif n<=19:
+    output=words_upto_19[n]
+elif n<=99:
+    output = words_upto_tens[n//10]+" "+words_upto_19[n%10]
+else:
+    print("Please enter value from 0-99 only...")
+print(output)
+Enter a digit from 0-99: 45
+FOURTY FIVE
 ***********************************************************************
 ***********************************************************************
