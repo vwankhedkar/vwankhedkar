236d60525cb74d66d07c69c67922be7b007c90e2
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-02 11:51:39 +0530

    Update BasicProgrammes.java

--- JAVA/BasicProgrammes.java
+++ JAVA/BasicProgrammes.java
@@ -461,7 +461,29 @@ public class RemoveDuplicates {
 	}
 }	===>		First non-repeated character : p
 *************************************************************************
-
+POJO
+package com.tryPrograms;
+public class Main {
+	private int id;
+	private String name;
+	private String department;
+	// Constructor
+	public Main (int id, String name, String department) {
+		this.id = id;
+		this.name = name;
+		this.department = department;
+	}
+	// Getter and setters
+	public int getId() { return id; }
+	public void setId(int id) { this.id = id; }
+	public String getName() {return name;}
+	public void setName(String name) {this.name = name; }
+	public String getDepartment() {return department;}
+	public void setDepartment(String department) {this.department = department; }
+	public static void main(String[] args) {
+	System.out.print("Hello");
+  } 
+}	
 ************************************************************************************************
 
 ************************************************************************************************
