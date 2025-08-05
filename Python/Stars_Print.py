adc1334b0c609933dea13b11f3489beec812e3d4
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-05 17:32:17 +0530

    Update Stars_Print.py

--- Python/Stars_Print.py
+++ Python/Stars_Print.py
@@ -58,6 +58,192 @@ OUTPUT - Enter a number: 5
 **
 *
 ---------------------------------------------------------------------------------------------
+n = 9
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * 
+n = 9
+for i in range(n):
+    for j in range(n):
+        if j == 0 or j == n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+n = 9
+for i in range(n):
+    for j in range(n):
+        if i==n//2 or j==n//2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+        *         
+        *         
+        *         
+        *         
+* * * * * * * * * 
+        *         
+        *         
+        *         
+        *    
+n = 9
+for i in range(n):
+    for j in range(n):
+        if i == j or i+j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+*               * 
+  *           *   
+    *       *     
+      *   *       
+        *         
+      *   *       
+    *       *     
+  *           *   
+*               * 
+n = 9
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+n = 9
+for i in range(n):
+    for j in range(n):
+        if j==0 or i==n-1 or i==j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+*                 
+* *               
+*   *             
+*     *           
+*       *         
+*         *       
+*           *     
+*             *   
+* * * * * * * * * 
+n = 9
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i+j==n-1:
+            print("*", end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+* * * * * * * * * 
+*             *   
+*           *     
+*         *       
+*       *         
+*     *           
+*   *             
+* *               
+*  
+n = 9
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        if j==0 or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(i+1):
+        if i==j or i==n-1:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+                  * 
+                *   * 
+              *       * 
+            *           * 
+          *               * 
+        *                   * 
+      *                       * 
+    *                           * 
+  * * * * * * * * * * * * * * * * * 
+n = 9
+for i in range(n-1):
+    for j in range(i,n-1):
+        print(" ",end=" ")
+    for j in range(i):
+        if j==0:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(i+1):
+        if i == j:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ",end=" ")
+    for j in range(i,n):
+        if j == i:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    for j in range(i,n-1):
+        if j == n-2:
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+                * 
+              *   * 
+            *       * 
+          *           * 
+        *               * 
+      *                   * 
+    *                       * 
+  *                           * 
+*                               * 
+  *                           * 
+    *                       * 
+      *                   * 
+        *               * 
+          *           * 
+            *       * 
+              *   * 
+                * 
 
 ---------------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------------
