c6bf0ecc6faab8a91d4f75d1852a9d6a1666a980
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-05 13:12:01 +0530

    Update and rename ActionChains.py to findLocators_Xpath.py

---
+++ Selenium/findLocators_Xpath.py
@@ -0,0 +1,21 @@
+
+page -> https://www.hyrtutorials.com/p/add-padding-to-containers.html#google_vignette
+
+Firstname text box
+//input[@name='name']/preceding-sibling::input[1]
+Email
+//label[text()='Email']/following-sibling::input[1]
+//label[text()='Email']/following-sibling::input[1]/parent::div
+password textbox
+//label[text()='Password']/following::input[1]
+tablename checkbox
+//td[text()='Maria Anders']/preceding-sibling::td/child::input
+Canada
+//td[text()='Yoshi Tannamuri']/following-sibling::td[1]
+All textboxes
+//div[@class='container']/child::input[@type='text']
+//div[@class='container']/child::input[@type='text'][1]
+All buttons
+//div[@class='container']/descendant::button
+whole frame boxes
+//div[@class='buttons']/ancestor-or-self::div
