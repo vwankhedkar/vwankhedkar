92cc2f0a808f728e8b4eeb7e2594c94c6b81ab91
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-08 10:57:57 +0530

    Update Sliding_window.py

--- Sliding_window.py
+++ Sliding_window.py
@@ -1,38 +1,20 @@
-import sys
-print "GFG"
-# O(n * k) solution for finding
-# maximum sum of a subarray of size k
-INT_MIN = -sys.maxsize - 1
- 
-# Returns maximum sum in a
-# subarray of size k.
- 
- 
 def maxSum(arr, n, k):
- 
-    # Initialize result
-    max_sum = INT_MIN
- 
-    # Consider all blocks
-    # starting with i.
+    max_sum = 0
+    current_sum = 0
+    lst = []
     for i in range(n - k + 1):
         current_sum = 0
+        lst.clear()
         for j in range(k):
             current_sum = current_sum + arr[i + j]
- 
-        # Update result if required.
+            lst.append(arr[i+j])
         max_sum = max(current_sum, max_sum)
- 
-    return max_sum
- 
- 
-# Driver code
-arr = [1, 4, 2, 10, 2,
-       3, 1, 0, 20]
+    return max_sum, lst
+arr = [1, 4, 2, 10, 2, 3, 1, 0, 20]
 k = 4
 n = len(arr)
-print(maxSum(arr, n, k))
-
+print(f"Maximum sum with {k} elements is : ", maxSum(arr, n, k))
+Maximum sum with 4 elements is :  (24, [3, 1, 0, 20])   
 *********************************************************
 # O(n) solution for finding
 # maximum sum of a subarray of size k
