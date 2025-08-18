a86e0eec62f9363461858f51917389300576ac5b
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-18 16:30:16 +0530

    Update Difficult_JavaProjs.java

--- JAVA/Difficult_JavaProjs.java
+++ JAVA/Difficult_JavaProjs.java
@@ -83,9 +83,84 @@ public class tryProgs2 {
     }
 }
 **********************************************************************************************
+Merge 2 sorted arrays
+import java.util.Arrays;
 
-
+class Main {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 7};
+        int[] arr2 = {7, 2, 5, 9};
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int[] merged = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                if (k == 0 || merged[k - 1] != arr1[i]) // avoid duplicates
+                    merged[k++] = arr1[i];
+                i++;
+            } else if (arr1[i] > arr2[j]) {
+                if (k == 0 || merged[k - 1] != arr2[j]) 
+                    merged[k++] = arr2[j];
+                j++;
+            } else { // arr1[i] == arr2[j]
+                if (k == 0 || merged[k - 1] != arr1[i])
+                    merged[k++] = arr1[i];
+                i++;
+                j++;
+            }
+        }
+        while (i < arr1.length) {
+            if (k == 0 || merged[k - 1] != arr1[i])
+                merged[k++] = arr1[i];
+            i++;
+        }
+        while (j < arr2.length) {
+            if (k == 0 || merged[k - 1] != arr2[j])
+                merged[k++] = arr2[j];
+            j++;
+        }
+        for (int x = 0; x < k; x++) {
+            System.out.print(merged[x] + " ");
+        }
+    }
+}
+1 2 3 5 7 9 
 **********************************************************************************************
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+class Main {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        int[] arr1 = {1, 3, 7};
+        int[] arr2 = {7, 2, 5, 9};
+        ArrayList<Integer> merged = new ArrayList<>();
+        int i=0, j=0, k=0;
+        while(i<arr1.length && j<arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                merged.add(arr1[i]);
+                i++;
+            } else {
+                merged.add(arr2[j]);
+                j++;
+            }
+        }
+        while (i < arr1.length) {
+            merged.add(arr1[i]);
+            j++;
+        }
+        while (j < arr2.length) {
+            merged.add(arr2[j]);
+            j++;
+        }
+        for (int num : merged) {
+            set.add(num);
+        }
+        for (int num : set)
+            System.out.print(num + " ");
+    }
+}    ===>    1 2 3 5 7 9
 **********************************************************************************************
 
 
