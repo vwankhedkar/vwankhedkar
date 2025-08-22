76db4c5d4bd4e8953bc60cd0b920d81095a911aa
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-22 17:24:52 +0530

    Update BasicProgrammes.java

--- JAVA/BasicProgrammes.java
+++ JAVA/BasicProgrammes.java
@@ -644,7 +644,27 @@ class Main {
     }
 }	==>	Palindrome
 ************************************************************************************************
-
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        Pattern pattern = Pattern.compile("^[a-z]{0,3}[0-9]{2,8}[A-Z]{3,}");
+        for(int i=0;i<n;i++)
+        {   Matcher matcher = pattern.matcher(scanner.nextLine());
+            if (matcher.find()) System.out.println("VALID");
+            else   System.out.println("INVALID");
+        }
+    }
+}
+2
+abc012333ABCDEEEE
+VALID
+0123AB
+INVALID
 ************************************************************************************************
 
 ************************************************************************************************
