831d51f48d21a18bc85f5f228c40ed14fa6c52a7
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-08-23 14:30:11 +0530

    Update regular_expr.py

--- Python/regular_expr.py
+++ Python/regular_expr.py
@@ -330,6 +330,25 @@ import re
 CountryCode=1,LocalAreaCode=877,Number=2638277
 CountryCode=91,LocalAreaCode=011,Number=23413627
 *******************************************************************************
+import re
+def is_valid_url(url):
+    regex = re.compile(
+        r'^(?:http|ftp)s?://'   # http:// or https:// or ftp://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'  # domain...
+        r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # ...including TLD
+        r'localhost|'                          # or localhost
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # or IPv4
+        r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'         # or IPv6
+        r'(?::\d+)?'                           # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)     # path
+    return re.match(regex, url) is not None
+input_url = input("Enter a URL: ")
+if is_valid_url(input_url):
+    print("Valid URL")
+else:
+    print("Invalid URL")
+Enter a URL: https://www.example123.com
+Valid URL
 *******************************************************************************
 *******************************************************************************
 *******************************************************************************
