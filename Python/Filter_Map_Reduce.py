b2f0ac37cfd42ad887c93a908dd74b5d0b8394db
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-09-02 11:49:38 +0530

    Update Filter_Map_Reduce.py

--- Python/Filter_Map_Reduce.py
+++ Python/Filter_Map_Reduce.py
@@ -1,3 +1,18 @@
+numbers = [1, 2, 3, 4, 5, 6 ]
+sqr = map(lambda x:x**2, numbers)
+print(list(sqr))
+
+even_num = filter(lambda x:x%2==0, numbers) 
+print(list(even_num))
+
+from functools import reduce
+numbers = [1, 2, 3, 4]
+product = reduce(lambda x, y: x * y, numbers)
+print(product)  
+[1, 4, 9, 16, 25, 36]
+[2, 4, 6]
+24
+*******************************************************************
 list1 = [1,2,3,4,5,6,7,8,9,10]
 def is_even(n):
     if n % 2 == 0:
