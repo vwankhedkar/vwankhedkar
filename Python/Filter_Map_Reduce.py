70d01369786aab262d738e6f90815a7792f505c9
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-09-02 14:37:48 +0530

    Update Filter_Map_Reduce.py

--- Python/Filter_Map_Reduce.py
+++ Python/Filter_Map_Reduce.py
@@ -24,3 +24,17 @@ f = list(filter(is_even, list1))
 print(f)
 
 OUTPUT   [2, 4, 6, 8, 10]
+**********************************************************************
+fruit = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+map_obj = map(lambda s:s[0] == 'A', fruit)
+print(list(map_obj))
+**********************************************************************
+from functools import reduce
+fruit = ["Apple", "Banana", "Pear", "Apricot", "Orange"]
+map_obj = map(lambda s:s[0] == 'A', fruit)
+print(list(map_obj))
+filter_obj = filter(lambda s:s[0] == 'A', fruit)
+print(list(filter_obj))
+reduce_obj = reduce(lambda x,y:x+ " " +y, fruit)
+print(reduce_obj)
+**********************************************************************
