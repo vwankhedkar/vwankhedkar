9f3f647428b8597df67cceb0c57c56d2f9428d62
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-09-14 11:50:46 +0530

    Update Stars_Traingles_Print.py

--- Python/Stars_Traingles_Print.py
+++ Python/Stars_Traingles_Print.py
@@ -1,4 +1,27 @@
-
+n = 5
+for i in range(1, n+1):
+    for j in range(n-i):
+        print(" ", end=" ")
+    for k in range(2*i-1):
+        print("*", end=" ")
+    print()
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+********************************************************
+n = 5
+for i in range(1, n):
+    print("*")
+for j in range(1, n):
+    print("* ", end="")
+*
+*
+*
+*
+* * * * 
+********************************************************
 n = 5
 for i in range(n):
     print(""*(n-i-1)+"*"*(2*i+1))
