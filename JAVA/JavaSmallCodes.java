90d835ed1b3d89eb559ced6fe9ed40a00d92315b
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-09-18 18:20:53 +0530

    Update JavaSmallCodes.java

--- JAVA/JavaSmallCodes.java
+++ JAVA/JavaSmallCodes.java
@@ -1654,6 +1654,21 @@ bca
 cab
 cba
 *********************************************************************************
+import java.util.*;  // Print the consecutive pairs in array having sum Equals target.
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 2, 5, 6, 7, 8, 2,1,2};
+        int target = 3;
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] + arr[i+1] == target) {
+                System.out.println("index:"+i+"+"+"index:"+(i+1)+"="+arr[i]+"+"+arr[i+1]);
+            }
+        }
+    }
+} 
+index:0+index:1=1+2
+index:9+index:10=2+1
+index:10+index:11=1+2
 *********************************************************************************
 
 *********************************************************************************
