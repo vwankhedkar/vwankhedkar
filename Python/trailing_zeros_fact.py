d570bf0814a7da9ff06b4d78173780faca9cda96
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-08 17:44:41 +0530

    Update trailing_zeros_fact.py

--- Python/trailing_zeros_fact.py
+++ Python/trailing_zeros_fact.py
@@ -1,11 +1,5 @@
 # Time:  O(logn) = O(1)
 # Space: O(1)
-#
-# Given an integer n, return the number of trailing zeroes in n!.
-# 
-# Note: Your solution should be in logarithmic time complexity.
-#
-
 class Solution:
     # @return an integer
     def trailingZeroes(self, n):
