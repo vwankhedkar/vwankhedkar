2de1a8bf2973f9460f07ce9cdc61e7de1b565b0e
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-14 08:26:05 +0530

    Update regular_expr.py

--- Python/regular_expr.py
+++ Python/regular_expr.py
@@ -311,6 +311,25 @@ print(formatted)
 2025-05-29 22:59:22                        
 *******************************************************************************
 import re
+def is_valid_url(url):
+    regex = re.compile(
+    r'^(?:http|ftp)s?://'
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+    r'localhost|'
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'
+    r'(?::\d+)?'
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    return re.match(regex, url) is not None
+input_url = input("Enter a URL: ")
+if is_valid_url(input_url):
+    print("Valid URL")
+else:
+    print("Invalid URL")
+OUTPUT - Enter a URL: https://www.onlinegdb.com/online_python_compiler#
+Valid URL
+*******************************************************************************
+import re
 regex = re.compile(r'(\d+)(?:-| )(\d+)(?:-| )(\d+)')
 n = int(input())
 for _ in range(n):
