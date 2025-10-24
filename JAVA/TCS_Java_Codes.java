318a7b17c1b5f0c06e603c620d1fc85b67ef43c2
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-24 11:33:03 +0530

    Update TCS_Java_Codes.java

--- JAVA/TCS_Java_Codes.java
+++ JAVA/TCS_Java_Codes.java
@@ -779,6 +779,42 @@ class Main {
     }
 }    =====>    commonSubSequence : 3
 *********************************************************************************
+// Time Complexity: O(m * n)
+// Space Complexity: O(m * n)
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        String str = "abcde";
+        String pattern = "a*c?e";
+        System.out.println("wildCardCharMatch : " + wildCardCharMatch(str, pattern));
+    }
+    public static boolean wildCardCharMatch(String str, String pattern) {
+        int m = str.length(), n = pattern.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[0][0] = true;
+        for (int j=1; j<=n; j++) {
+            if (pattern.charAt(j-1)=='*')
+               dp[0][j] = dp[0][j-1];
+        }
+        for (int i=1; i<=m; i++) {
+            for (int j=1; j<=n; j++) {
+                if (pattern.charAt(j-1) == '?' || pattern.charAt(j-1)== str.charAt(i-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else if (pattern.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}    ====>    wildCardCharMatch : true
+*********************************************************************************
+*********************************************************************************
+*********************************************************************************
+
+*********************************************************************************
+*********************************************************************************
+*********************************************************************************
 
 *********************************************************************************
 *********************************************************************************
