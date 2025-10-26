7092af8f68c0b43f0d5a5e6c043df880bb74f15e
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-26 21:56:04 +0530

    Update predefinedMethods.py

--- Python/predefinedMethods.py
+++ Python/predefinedMethods.py
@@ -403,7 +403,20 @@ password_length = 12
 print("Generate Password : ",generate_password(password_length))
 Generate Password :  [HtRq|byMX}=
 ***************************************************************************
-
+import socket
+def is_valid_ip(ip):
+    try:
+        socket.inet_aton(ip)
+        return True
+    except socket.error:
+        return False
+ip_address = input("Enter an IP address")
+if is_valid_ip(ip_address):
+    print("Valid IP address")
+else:
+    print("Invalid IP address")
+Enter an IP address 121.34.56.190
+Invalid IP address
 ***************************************************************************
 ***************************************************************************
 ***************************************************************************
