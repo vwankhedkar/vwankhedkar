61e77b3c8c72e6246b6a4ce9ec70243953657744
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-29 12:11:48 +0530

    Update Codes_36.java

--- JAVA/Codes_36.java
+++ JAVA/Codes_36.java
@@ -139,7 +139,27 @@ public class Main
 	}
 }	==>	Sum of the integers in array : 26
 *************************************************************************************************
-  
+public class Main
+{
+	public static void main(String[] args) {
+	    int[] array = {5, 2, 9, 1, 6, 3};
+	    int target = 6;
+        int index= linearSearch(array, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index : " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+	}
+	public static int linearSearch(int[] array, int target) {
+	    for (int i=0; i<array.length; i++) {
+	        if (array[i] == target) {
+	            return i;
+	        }
+	    }
+	    return -1;
+	}
+} ==>	Element 6 found at index : 4
 *************************************************************************************************
 
 *************************************************************************************************
