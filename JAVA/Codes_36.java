0b8b77d05f039cf64b99d51d87476970574a5779
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2025-10-29 16:17:49 +0530

    Update Codes_36.java

--- JAVA/Codes_36.java
+++ JAVA/Codes_36.java
@@ -205,7 +205,33 @@ public class Main
 	}
 }	===>	The missing number is : 3
 *************************************************************************************************
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+public class Main
+{
+	public static void main(String[] args) {
+	    ArrayList<Integer> arrayList = new ArrayList<>();
+	    arrayList.add(5);
+	    arrayList.add(2);
+	    arrayList.add(9);
+	    arrayList.add(1);
+	    arrayList.add(6);
+	    arrayList.add(2);
+	    arrayList.add(5);
+	    ArrayList<Integer> uniqueList = removeDuplicates(arrayList);
+        System.out.println("Arraylist with duplicates removed");
+        for (int num : uniqueList) {
+            System.out.print(num + " ");
+        }
+	}
+	public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
+		Set<Integer> lstSet = new HashSet<>(list);
+		return new ArrayList<>(lstSet);
+	}
+}	
+Arraylist with duplicates removed
+1 2 5 6 9 
 *************************************************************************************************
 
 *************************************************************************************************
