3b88907f428581951a9412777b49f775ac64ff77
vwankhedkar <48506217+vwankhedkar@users.noreply.github.com>
2026-05-29 21:16:46 +0530

    Update All_Python_Codes.py

--- Python/All_Python_Codes.py
+++ Python/All_Python_Codes.py
@@ -2657,3 +2657,12 @@ Enter time in years: 10
 Enter number of times interest applied per year: 12
 Compound Interest Amount: 392039.88
 ---------------------------------------------------
+def to_camel_case(text):
+    removed = text.replace('-', ' ').replace('_', ' ').split()
+    if len(removed) == 0:
+        return ''
+    return removed[0]+ ''.join([x.capitalize() for x in removed[1:]])
+
+print(to_camel_case("the-stealth-warrior"))
+print(to_camel_case("abcd_efgh_ijkl"))
+---------------------------------------------------
